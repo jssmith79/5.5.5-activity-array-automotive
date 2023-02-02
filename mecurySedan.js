042f5af72ad288292d826cf3f0c89cf0a79d2d13
jssmith79 <i.am.hef@gmail.com>
2023-02-01 20:05:16 -0800

    resubmission

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,58 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class SedanDetails extends Vehicle{
+    constructor(name, maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService){
+        super(name, maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService);
+        this.name = name;
+        this.maximumPassengers = maximumPassengers;
+        this.passenger = passenger;
+        this.numberOfWheels = numberOfWheels;
+        this.maximumSpeed = maximumSpeed;
+        this.fuel = fuel;
+        this.scheduleService = scheduleService;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+
+}
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
--- vehicle.js
+++ vehicle.js
@@ -12,113 +12,59 @@ class Vehicle {
     }
 
 //extending Vehicle class properties for step 6.  It does not like the class name I made Sedan Details? I'm missing something.
-class SedanDetails extends Vehicle{
-    constructor(name, maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService){
-        this.name = name;
-        this.maximumPassengers = maximumPassengers;
-        this.passenger = passenger;
-        this.numberOfWheels = numberOfWheels;
-        this.maximumSpeed = maximumSpeed;
-        this.fuel = fuel;
-        this.scheduleService = scheduleService;
-    }
-    super(name, maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService)
-//adding the additional methods -- or trying to at least :)
-    loadPassenger(num){
-        if (this.passenger <= this.maximumPassengers){
-            return console.log("Enjoy the ride")
-        } else {
-            return console.log("You ain't gonna fit!")
-    }
-    }
 
-}
-    start() {
+ 
+accelerate() {
+    if (this.started) {
         if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
+            console.log(this.speed += 1);
+            this.fuel = this.fuel - 1;
         } else {
-            return this.started = false;
-            console.log("engine cannot start...");
+            console.log("out of fuel.");
+            this.stop();
         }
+    } else {
+        alert("You need to start the engine first.");
     }
+}
+//this would work correctly.  
 
-//it does not like the name I gave the schedule service method I gave as well.  
-    scheduleService(mileage){
-        if (mileage === 30000){
-            return this.maintenance = true;
-            console.log("Schedule maintenance soon")
-        } else {
-            return this.maintenance = false;
-            console.log("Maintenance not due yet")
-        }
-    }
-    accelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                console.log(this.speed += 1);
+decelerate() {
+    if (this.started) {
+        if (this.fuel > 0) {
+            if (this.speed > 0) {
+                console.log(this.speed -= 1);
                 this.fuel = this.fuel - 1;
             } else {
-                console.log("out of fuel.");
-                this.stop();
+                console.log(this + " has stopped moving");
+                this.fuel = this.fuel - 1;
             }
         } else {
-            alert("You need to start the engine first.");
+            console.log("out of fuel.");
+            this.stop();
         }
+    } else {
+        alert("You need to start the engine first.");
     }
-    decelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                if (this.speed > 0) {
-                    console.log(this.speed -= 1);
-                    this.fuel = this.fuel - 1;
-                } else {
-                    console.log(this + " has stopped moving");
-                    this.fuel = this.fuel - 1;
-                }
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
-        } else {
-            alert("You need to start the engine first.");
-        }
-    }
-    stop() {
-        this.started = false;
-    }
+}
+//will stop the function from working.
+stop() {
+    console.log('engine off')
+    this.started = false;
+}
 
-    drive() {
-        accelerate();
+typeOfVehicle() {
+    if (this.numberOfWheels == 8) {
+        console.log(this.model + " " + this.make + " is a Truck");
+    } else if (this.numberOfWheels == 4) {
+        console.log(this.model + " " + this.make + " is a Car");
+    } else if (this.numberOfWheels == 2) {
+        console.log(this.model + " " + this.make + " is a Bike");
+    } else {
+        console.log("Unknown type of vehicle");
     }
-    brake() {
-        decelerate();
-    }
-
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
-
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
-            console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
-            console.log(this.model + " " + this.make + " is a Bike");
-        } else {
-            console.log("Unknown type of vehicle");
-        }
-    }
+}
 }
 
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+module.exports = { Vehicle }
